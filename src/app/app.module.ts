27677dfff1110973f50e44f4aac11ab04c152c84
ashleylovette <ashleylovette15@gmail.com>
2021-10-10 18:30:27 -0500

    Angular exercise using databinding and structural directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllOtherOrdersComponent } from './all-other-orders/all-other-orders.component';
+import { FirstFiveOrdersComponent } from './first-five-orders/first-five-orders.component';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { LotteryWinningOrderComponent } from './lottery-winning-order/lottery-winning-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderDashboardComponent,
+    FirstFiveOrdersComponent,
+    AllOtherOrdersComponent,
+    LotteryWinningOrderComponent
   ],
   imports: [
     BrowserModule
